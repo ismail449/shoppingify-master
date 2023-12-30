0cf333a09aa8d8a41e6bffbba6fd7a372b1ba353
ismail <esmael449@stud.fci-cu.edu.eg>
2023-12-30 21:13:23 +0200

    added the design of the nav-bar component

--- components/nav-bar/page.tsx
+++ components/nav-bar/page.tsx
@@ -1,7 +1,50 @@
 import React from "react";
+import Image from "next/image";
+import styles from "./nav-bar.module.css";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <nav className={styles.navBar}>
+      <Image
+        className={styles.logo}
+        src="./logo.svg"
+        width={41}
+        height={41}
+        alt="logo"
+        priority
+      />
+      <ul className={styles.navigationItems}>
+        <li>
+          <Image src="./list.svg" width={26} height={26} alt="a list icon" />
+        </li>
+        <li>
+          <Image
+            src="./history.svg"
+            width={26}
+            height={26}
+            alt="a history icon"
+          />
+        </li>
+        <li>
+          <Image
+            src="./statistics.svg"
+            width={26}
+            height={26}
+            alt="a list icon"
+          />
+        </li>
+      </ul>
+      <div className={styles.shoppingCartContiner}>
+        <Image
+          src="./shopping_cart.svg"
+          width={20}
+          height={20}
+          alt="logo"
+          priority
+        />
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
