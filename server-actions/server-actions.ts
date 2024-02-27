09e519bf9d7bb1ec3dcca20f0c236820d03fc8fd
ismail <esmael449@stud.fci-cu.edu.eg>
2024-02-27 19:00:53 +0200

    moved the custom getServerSession function to be in separate folder

--- server-actions/server-actions.ts
+++ server-actions/server-actions.ts
@@ -1,25 +1,8 @@
 "use server";
 import { prisma } from "@/lib/prisma";
+import { getServerSession } from "@/lib/getServerSession";
 
-import { cookies, headers } from 'next/headers';
-import { getServerSession as originalGetServerSession } from 'next-auth';
-import { authOptions } from "@/app/api/auth/[...nextauth]/route"; 
 
-export const getServerSession = async () => {
-  const req = {
-    headers: Object.fromEntries(headers() as Headers),
-    cookies: Object.fromEntries(
-      cookies()
-        .getAll()
-        .map((c) => [c.name, c.value]),
-    ),
-  };
-  const res = { getHeader() {}, setCookie() {}, setHeader() {} };
-
-  // @ts-ignore - The type used in next-auth for the req object doesn't match, but it still works 
-  const session = await originalGetServerSession(req, res, authOptions);
-  return session;
-};
 
 export const addItem = async (formData: FormData) => {
   const session = await getServerSession();
