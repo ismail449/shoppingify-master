754a073153f0dbe6ad676db9e0bb2cc69a3c3c19
ismail <esmael449@stud.fci-cu.edu.eg>
2024-03-02 20:06:52 +0200

    moved the user getting logic to be in a function

--- server-actions/server-actions.ts
+++ server-actions/server-actions.ts
@@ -2,55 +2,80 @@
 import { prisma } from "@/lib/prisma";
 import { getServerSession } from "@/lib/getServerSession";
 import { revalidatePath } from "next/cache";
+import { signOut } from "next-auth/react";
 
-export const addItem = async (previousState: any,formData: FormData) => {
+const getUser = async () => {
   const session = await getServerSession();
-  
+
   const userEmail = session?.user?.email;
-  if(!userEmail){
-    return { message: "User email not found", isError: true };
+  if (!userEmail) {
+    await signOut();
+    return;
   }
-  console.log(session)
 
   const user = await prisma.user.findUnique({ where: { email: userEmail } });
-  if(!user){
-    return { message: "User not found", isError: true };
+  if (!user) {
+    await signOut();
+    return;
+  }
+  return user;
+};
+
+export const addItem = async (previousState: any, formData: FormData) => {
+  const user = await getUser();
+  if (!user) {
+    return { message: "User is not found", isError: true };
   }
   const category = (formData.get("category") as string).toLocaleLowerCase();
   const item = (formData.get("name") as string).toLocaleLowerCase();
   const note = (formData.get("note") as string).toLocaleLowerCase();
   const imageUrl = (formData.get("image") as string).toLocaleLowerCase();
 
-
-  let dbItem = await prisma.item.findUnique({where: { name: item }});
-  let dbCategory = await prisma.category.findUnique({ where: { name: category } });
+  let dbItem = await prisma.item.findUnique({ where: { name: item } });
+  let dbCategory = await prisma.category.findUnique({
+    where: { name: category },
+  });
 
   if (!dbCategory && user) {
-    const newCategory = await prisma.category.create({ data: { name: category } });
+    const newCategory = await prisma.category.create({
+      data: { name: category },
+    });
     await prisma.categoriesOnUsers.create({
-      data:{
+      data: {
         categoryId: newCategory.id,
-        userId: user.id
-      }
-    })
-    dbCategory = await prisma.category.findUnique({ where: { name: category } });
+        userId: user.id,
+      },
+    });
+    dbCategory = await prisma.category.findUnique({
+      where: { name: category },
+    });
   }
-  if(!dbCategory?.id){
-    return { message: "Error creating a new category", isError: true  };
+  if (!dbCategory?.id) {
+    return { message: "Error creating a new category", isError: true };
   }
 
-  let dbCategoryOnUser = await prisma.categoriesOnUsers.findUnique({where: { userId_categoryId: {categoryId: dbCategory.id, userId: user.id} }});
-  if(!dbCategoryOnUser){
-    dbCategoryOnUser = await prisma.categoriesOnUsers.create({data: { categoryId: dbCategory.id, userId: user.id }});
+  let dbCategoryOnUser = await prisma.categoriesOnUsers.findUnique({
+    where: {
+      userId_categoryId: { categoryId: dbCategory.id, userId: user.id },
+    },
+  });
+  if (!dbCategoryOnUser) {
+    dbCategoryOnUser = await prisma.categoriesOnUsers.create({
+      data: { categoryId: dbCategory.id, userId: user.id },
+    });
   }
-  
-  if(!dbItem) {
-    dbItem = await prisma.item.create({ data: { name: item, categoryId: dbCategory?.id, imageUrl, note } })
-    revalidatePath("/")
-    return { message: "Item added successfully", isError: false  };
+
+  if (!dbItem) {
+    dbItem = await prisma.item.create({
+      data: { name: item, categoryId: dbCategory?.id, imageUrl, note },
+    });
+    revalidatePath("/");
+    return { message: "Item added successfully", isError: false };
   }
-  await prisma.item.update({where: {id: dbItem.id}, data: { categoryId: dbCategory?.id, imageUrl, note }})
-  revalidatePath("/")
+  await prisma.item.update({
+    where: { id: dbItem.id },
+    data: { categoryId: dbCategory?.id, imageUrl, note },
+  });
+  revalidatePath("/");
   return { message: "Item updated successfully", isError: false };
-
 };
