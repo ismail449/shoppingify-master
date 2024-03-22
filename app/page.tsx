d497b940a6d75099bdd7cbe157fcf3179c79b96e
ismail <esmael449@stud.fci-cu.edu.eg>
2024-03-22 11:41:41 +0200

    added the logic to fetch the items in the home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,10 @@ import ShoppingItem from "@/components/shopping-item/shopping-item";
 import { redirect } from "next/navigation";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
-import styles from "./page.module.css";
 import ShoppingList from "@/components/side-bar/shopping-list/shopping-list";
 import AddItemForm from "@/components/side-bar/add-item-form/add-item-form";
+import { prisma } from "@/lib/prisma";
+import styles from "./page.module.css";
 
 export default async function Home({
   searchParams,
@@ -13,9 +14,21 @@ export default async function Home({
   searchParams: { [key: string]: string | string[] | undefined };
 }) {
   const session = await getServerSession(authOptions);
-  if (!session) {
+  if (!session || !session.user?.email) {
     redirect("/api/auth/signin");
   }
+  const userEmail = session.user?.email;
+  const user = await prisma.user.findUnique({ where: { email: userEmail } });
+  const categoryOnUser = await prisma.categoriesOnUsers.findMany({
+    where: { userId: user?.id },
+  });
+  const items = await prisma.item.findMany({
+    where: {
+      categoryId: {
+        in: categoryOnUser.map((categoryOnUser) => categoryOnUser.categoryId),
+      },
+    },
+  });
   return (
     <main className={styles.home}>
       <div className={styles.topSectionContiner}>
@@ -27,7 +40,11 @@ export default async function Home({
           <SearchInput placeholder="search item" />
         </div>
       </div>
-      <ShoppingItem shoppingItem="Chicken leg box" />
+      {!!items.length
+        ? items.map((item) => (
+            <ShoppingItem key={item.id} shoppingItem={item.name} />
+          ))
+        : null}
       {!searchParams.shoppingSidebar ? <ShoppingList /> : null}
       {searchParams.shoppingSidebar === "add-item" ? <AddItemForm /> : null}
     </main>
