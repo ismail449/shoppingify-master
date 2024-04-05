f9b1ff4f07a5ddcbd32d2075b630ee1cd6b776c6
ismail <esmael449@stud.fci-cu.edu.eg>
2024-04-05 11:26:40 +0200

    added the back navigation link to the ShoppingItemDetails component

--- components/side-bar/shopping-item-details/shopping-item-details.tsx
+++ components/side-bar/shopping-item-details/shopping-item-details.tsx
@@ -1,5 +1,8 @@
 import React from "react";
+import Image from "next/image";
 import SideBar from "../side-bar";
+import styles from "./shopping-item-details.module.css";
+import Link from "next/link";
 
 const ShoppingItemDetails = ({
   searchParams,
@@ -7,7 +10,23 @@ const ShoppingItemDetails = ({
   searchParams: { [key: string]: string | string[] | undefined };
 }) => {
   console.log(searchParams.id);
-  return <SideBar>ShoppingItemDetails</SideBar>;
+  return (
+    <SideBar>
+      <div className={styles.shoppingItemDetails}>
+        <div className={styles.shoppingItemDatailsContainer}>
+          <Link href="/" className={styles.backNavigationContianer}>
+            <Image
+              src="./arrow_left_alt.svg"
+              alt="back navigation arrow"
+              width={24}
+              height={24}
+            />
+            <span>back</span>
+          </Link>
+        </div>
+      </div>
+    </SideBar>
+  );
 };
 
 export default ShoppingItemDetails;
