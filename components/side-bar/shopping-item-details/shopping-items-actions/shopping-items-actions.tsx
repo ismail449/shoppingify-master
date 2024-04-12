bf7af781da927ef51bd136d202f9256422109bfc
ismail <esmael449@stud.fci-cu.edu.eg>
2024-04-12 21:09:16 +0200

    edited the ShoppingItemasActions to show loading state and to redirect the user when the delete is done

--- components/side-bar/shopping-item-details/shopping-items-actions/shopping-items-actions.tsx
+++ components/side-bar/shopping-item-details/shopping-items-actions/shopping-items-actions.tsx
@@ -1,23 +1,46 @@
 "use client";
 import React, { FC } from "react";
+import { useFormState } from "react-dom";
 import Button from "@/components/button/button";
 import { deleteShoppingItem } from "@/server-actions/server-actions";
 import styles from "./shopping-items-actions.module.css";
+import { useRouter, usePathname } from "next/navigation";
 
 type ShoppingItemsProps = {
   itemId: string;
 };
 
+const initialFormState = {
+  isError: false,
+  message: "",
+};
+
 const ShoppingItemasActions: FC<ShoppingItemsProps> = ({ itemId }) => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const handleFormSubmit = async (previousState: any, formData: FormData) => {
+    const actionResponse = await deleteShoppingItem(previousState, formData);
+    if (actionResponse?.isError) {
+      return actionResponse;
+    }
+    router.push(pathname);
+    return actionResponse;
+  };
+
+  const [state, formAction] = useFormState(handleFormSubmit, initialFormState);
+
   return (
-    <div className={styles.buttonsContainer}>
-      <Button buttonType="cancel" onClick={() => deleteShoppingItem(itemId)}>
-        Delete
-      </Button>
-      <Button type="submit" buttonType="primary">
-        Add to list
-      </Button>
-    </div>
+    <form action={formAction}>
+      <span>{state?.isError ? state.message : null}</span>
+      <div className={styles.buttonsContainer}>
+        <Button value={itemId} name="delete" buttonType="cancel">
+          Delete
+        </Button>
+        <Button type="submit" buttonType="primary">
+          Add to list
+        </Button>
+      </div>
+    </form>
   );
 };
 
