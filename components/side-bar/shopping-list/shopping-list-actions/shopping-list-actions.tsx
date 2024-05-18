c5f51e8fe3fc888520a4bd8adc4cf72fd1dfa21c
ismail <esmael449@stud.fci-cu.edu.eg>
2024-05-18 21:36:17 +0300

    added the logic to open the CancelListModal from the ShoppingListActions

--- components/side-bar/shopping-list/shopping-list-actions/shopping-list-actions.tsx
+++ components/side-bar/shopping-list/shopping-list-actions/shopping-list-actions.tsx
@@ -1,9 +1,10 @@
 "use client";
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import Button from "@/components/button/button";
 import ButtonsActions from "@/components/buttons-actions/buttons-actions";
 import {} from "@/server-actions/server-actions";
 import { useRouter, usePathname } from "next/navigation";
+import CancelListModal from "@/components/cancel-list-modal/cancel-list-modal";
 
 type Props = {
   shoppingListId: string;
@@ -15,22 +16,40 @@ const initialFormState = {
 };
 
 const ShoppingListActions: FC<Props> = ({ shoppingListId }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const router = useRouter();
   const pathname = usePathname();
-  const handleFormSubmit = async (previousState: any, formData: FormData) => {};
+  const handleFormSubmit = async (previousState: any, formData: FormData) => {
+    const shoppingListId = formData.get("complete") as string;
+    if (!shoppingListId) {
+      setIsModalOpen(true);
+    }
+  };
+
+  const handleOnModalClose = () => {
+    setIsModalOpen(false);
+  };
 
   return (
-    <ButtonsActions
-      handleFormSubmit={handleFormSubmit}
-      initialFormState={initialFormState}
-    >
-      <Button name="complete" buttonType="transparent">
-        cancel
-      </Button>
-      <Button type="submit" buttonType="complete">
-        Complete
-      </Button>
-    </ButtonsActions>
+    <>
+      <ButtonsActions
+        handleFormSubmit={handleFormSubmit}
+        initialFormState={initialFormState}
+      >
+        <Button name="cancel" buttonType="transparent">
+          cancel
+        </Button>
+        <Button
+          type="submit"
+          value={shoppingListId}
+          name="complete"
+          buttonType="complete"
+        >
+          Complete
+        </Button>
+      </ButtonsActions>
+      <CancelListModal isOpen={isModalOpen} onClose={handleOnModalClose} />
+    </>
   );
 };
 
