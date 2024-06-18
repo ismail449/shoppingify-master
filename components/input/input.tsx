1c4173fb09a24710a2d7ffcdc5032e962047d5a6
ismail <esmael449@stud.fci-cu.edu.eg>
2024-06-18 23:48:27 +0300

    refactored the input component

--- components/input/input.tsx
+++ components/input/input.tsx
@@ -1,130 +1,53 @@
-"use client";
-import React, { FC, useRef, useState } from "react";
+import React, { ComponentPropsWithoutRef, FC } from "react";
 import Image from "next/image";
-import useClickedOutside from "@/hooks/useClickedOutside";
 import styles from "./input.module.css";
 
-type Props = {
+type CommonProps = {
   textarea?: boolean;
-  buttonProps?: ButtonProps;
-  labelProps?: LabelProps;
-  disabled?: boolean;
-  placeholder?: string;
-  required?: boolean;
-  type?: "text" | "url";
-  categoryList?: string[];
-  name?: string;
+  label?: string;
+  children?: React.ReactNode;
 };
 
-type LabelProps = {
-  label: string;
-  id: string;
-};
+type InputProps = CommonProps & ComponentPropsWithoutRef<"input">;
+type TextareaProps = CommonProps & ComponentPropsWithoutRef<"textarea">;
 
-type ButtonProps = {
-  buttonText: string;
-  buttonOnClick: () => void;
-};
-
-const Input: FC<Props> = ({
-  buttonProps,
-  labelProps,
-  disabled = false,
+const Input: FC<InputProps | TextareaProps> = ({
+  label = "",
   textarea = false,
-  required = false,
-  type = "text",
-  categoryList = [],
-  placeholder = "",
-  name = "",
+  children,
+  ...otherProps
 }) => {
-  const [isFocused, setIsFocused] = useState(false);
-  const [inputValue, setInputValue] = useState("");
-
-  const inputWrapperRef = useRef<HTMLDivElement>(null);
-
-  const clickedOutSide = useClickedOutside(inputWrapperRef);
-
-  const filteredCategoryList = categoryList.filter((categoryItem: string) =>
-    categoryItem.toLocaleLowerCase().includes(inputValue.toLocaleLowerCase())
-  );
-
-  const onCategoryItemSelect = (selectedItem: string) => {
-    setInputValue(selectedItem);
-  };
-
-  const onInputValueChange = (
-    e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
-  ) => {
-    setInputValue(e.target.value);
-  };
-
-  const clearInput = () => {
-    setInputValue("");
-  };
-  const otherProps = {
-    className: styles.input,
-    disabled,
-    id: labelProps?.id,
-    onFocus: () => setIsFocused(true),
-    onBlur: () => setIsFocused(false),
-    onChange: onInputValueChange,
-    value: inputValue,
-    placeholder,
-    name,
-    required,
-  };
+  const inputProps = textarea
+    ? {}
+    : (otherProps as ComponentPropsWithoutRef<"input">);
+  const textareaProps = textarea
+    ? (otherProps as ComponentPropsWithoutRef<"textarea">)
+    : {};
   return (
-    <div className={styles.input} ref={inputWrapperRef}>
-      {labelProps?.label ? (
-        <label
-          className={`${styles.label} ${isFocused ? styles.focused : ""}`}
-          htmlFor={labelProps?.id}
-        >
-          {`${labelProps.label} ${!required ? "(optional)" : ""}`}
-        </label>
-      ) : null}
-      <div className={styles.inputContainer}>
-        {!textarea ? (
-          <input type={type} {...otherProps} autoComplete="off" />
-        ) : (
-          <textarea {...otherProps} />
-        )}
-        {inputValue.length > 0 ? (
-          <Image
-            className={styles.clearInput}
-            src="./close.svg"
-            alt="clear input value button"
-            width={19}
-            height={19}
-            onClick={clearInput}
-          />
-        ) : null}
-        {!!buttonProps?.buttonText ? (
-          <button
-            onClick={buttonProps.buttonOnClick}
-            disabled={disabled}
-            className={styles.inputButton}
-          >
-            {buttonProps.buttonText}
-          </button>
-        ) : null}
-      </div>
-      {filteredCategoryList.length > 0 && !clickedOutSide ? (
-        <div className={styles.categoryList}>
-          {filteredCategoryList.map((categoryItem) => {
-            return (
-              <div
-                onClick={() => onCategoryItemSelect(categoryItem)}
-                className={styles.categoryItem}
-                key={categoryItem}
-              >
-                {categoryItem}
-              </div>
-            );
-          })}
+    <>
+      <label className={`${styles.input} ${styles.label}`}>
+        <span>
+          {label} {!otherProps.required ? "(optional)" : ""}
+        </span>
+        <div className={styles.inputContainer}>
+          {!textarea ? (
+            <input {...inputProps} autoComplete="off" />
+          ) : (
+            <textarea {...textareaProps} />
+          )}
+          {otherProps.value ? (
+            <Image
+              className={styles.clearInput}
+              src="./close.svg"
+              alt="clear input value button"
+              width={19}
+              height={19}
+            />
+          ) : null}
+          {children}
         </div>
-      ) : null}
-    </div>
+      </label>
+    </>
   );
 };
 
