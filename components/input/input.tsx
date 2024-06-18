00f2e495ec4027f3af301341776225d0bcae0172
ismail <esmael449@stud.fci-cu.edu.eg>
2024-06-19 01:02:42 +0300

    removed the clear input functionality

--- components/input/input.tsx
+++ components/input/input.tsx
@@ -1,5 +1,4 @@
 import React, { ComponentPropsWithoutRef, FC } from "react";
-import Image from "next/image";
 import styles from "./input.module.css";
 
 type CommonProps = {
@@ -8,7 +7,7 @@ type CommonProps = {
   children?: React.ReactNode;
 };
 
-type InputProps = CommonProps & ComponentPropsWithoutRef<"input">;
+export type InputProps = CommonProps & ComponentPropsWithoutRef<"input">;
 type TextareaProps = CommonProps & ComponentPropsWithoutRef<"textarea">;
 
 const Input: FC<InputProps | TextareaProps> = ({
@@ -31,19 +30,10 @@ const Input: FC<InputProps | TextareaProps> = ({
         </span>
         <div className={styles.inputContainer}>
           {!textarea ? (
-            <input {...inputProps} autoComplete="off" />
+            <input {...inputProps} />
           ) : (
             <textarea {...textareaProps} />
           )}
-          {otherProps.value ? (
-            <Image
-              className={styles.clearInput}
-              src="./close.svg"
-              alt="clear input value button"
-              width={19}
-              height={19}
-            />
-          ) : null}
           {children}
         </div>
       </label>
