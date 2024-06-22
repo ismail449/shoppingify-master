dd4026f660ceb32673ccd42b1022ad78f1c9d182
ismail <esmael449@stud.fci-cu.edu.eg>
2024-06-22 20:44:33 +0300

    added a notification badge to the ShoppingCartIcon component

--- components/shopping-cart-icon/shopping-cart-icon.tsx
+++ components/shopping-cart-icon/shopping-cart-icon.tsx
@@ -3,12 +3,23 @@
 import React from "react";
 import Image from "next/image";
 import { useOpenSidebarContext } from "@/context/open-sidebar-context";
+import { useShoppingListContext } from "@/context/shopping-list-context";
 import styles from "./shopping-cart-icon.module.css";
 
 const ShoppingCartIcon = () => {
   const { toggleSidebar } = useOpenSidebarContext();
+  const { shoppingList } = useShoppingListContext();
+  const shoppingListItemsCount = shoppingList
+    .map((item) => item.itemCount)
+    .reduce((prev, curr) => prev + curr, 0);
+  console.log(shoppingListItemsCount);
   return (
     <div onClick={toggleSidebar} className={styles.shoppingCartContiner}>
+      {shoppingListItemsCount > 0 ? (
+        <span className={styles.shoppingCartCount}>
+          {shoppingListItemsCount}
+        </span>
+      ) : null}
       <Image
         src="./shopping_cart.svg"
         width={20}
