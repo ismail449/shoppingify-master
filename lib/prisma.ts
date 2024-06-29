6bd9fbeb7adade5fb363dfef61854844ba713ec0
ismail <esmael449@stud.fci-cu.edu.eg>
2024-06-29 23:13:55 +0300

    added a fix for a prisma issue with sending data fetched useing prisma to a client component from a server component

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,3 +1,14 @@
 import { PrismaClient } from "@prisma/client";
-export const prisma = new PrismaClient()
 
+/*This is a fix for this issue https://github.com/prisma/prisma/issues/20691 which caused prisma to give an error in dvelopment*/
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+export const prisma: PrismaClient =
+  typeof window === "undefined"
+    ? globalForPrisma.prisma ?? new PrismaClient()
+    : (undefined as unknown as PrismaClient);
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
