8870a01e7acb5c1f0f74072ddef357ef2ff5cfc8
ismail <esmael449@stud.fci-cu.edu.eg>
2024-07-06 17:10:25 +0300

    refactored the groupArrayByCatigory function to be more reusable

--- utils/index.ts
+++ utils/index.ts
@@ -1,13 +1,15 @@
-import { ShoppingItem } from "@prisma/client";
-
-type ShoppingListGroupedByCategory = {
-  [category: string]: { items: ShoppingItem[] };
+type ShoppingListGroupedByCategory<T> = {
+  [category: string]: {
+    items: T[];
+  };
 };
-export const groupArrayByCatigory = (array: ShoppingItem[]) => {
+export const groupArrayByCatigory = <T extends { categoryName: string }>(
+  array: T[]
+) => {
   return array.reduce((acc, item) => {
     const category = item.categoryName;
     acc[category] = acc[category] ?? { items: [] };
     acc[category].items.push(item);
     return acc;
-  }, {} as ShoppingListGroupedByCategory);
+  }, {} as ShoppingListGroupedByCategory<T>);
 };
